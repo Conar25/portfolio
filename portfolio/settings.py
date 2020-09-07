b6c85717d316e5cf49d281f1f50ae3263299f510
Conar <galiev25@list.ru>
2020-09-07 16:16:10 +0300

    stattic files fix

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
 
 # # AWS Bucket settings
 
