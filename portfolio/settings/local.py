2b8d46af12fbebb2271e32da3e0e06185e7f0386
Conar <galiev25@list.ru>
2020-09-23 19:29:05 +0300

    Postgres database in local environment added

--- portfolio/settings/local.py
+++ portfolio/settings/local.py
@@ -6,7 +6,11 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DATABASE_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
