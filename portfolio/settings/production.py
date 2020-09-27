8e9929785891161a7c2dfb5a5347f0dcc358878c
Conar <galiev25@list.ru>
2020-09-27 14:56:51 +0300

    fixtures added

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -4,5 +4,4 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['ilshat-profile.herokuapp.com']
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
